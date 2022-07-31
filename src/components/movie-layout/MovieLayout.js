1225094a5a4027b0da9930c053b050f4cd5b61a5
Soner <80179760+sonerady@users.noreply.github.com>
2022-08-01 00:37:50 +0300

    created movie list beta

--- src/components/movie-layout/MovieLayout.js
+++ src/components/movie-layout/MovieLayout.js
@@ -1,15 +1,52 @@
 import React, { useState, useEffect } from "react";
 import styles from "./movieLayout.module.scss";
 import MovieCard from "../../components/movie-card/MovieCard";
+import tmdbApi, { category, movieType } from "../../api/tmdbApi";
+import { useParams } from "react-router-dom";
 
-const MovieLayout = () => {
+const MovieLayout = (props) => {
   const [items, setItems] = useState([]);
 
   const [page, setPage] = useState(1);
 
   const [totalPage, setTotalPage] = useState(0);
 
-  return <div>MovieLayout</div>;
+  const { keyword } = useParams();
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      if (keyword === undefined) {
+        const params = {};
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(movieType.upcoming, {
+              params,
+            });
+            break;
+          default:
+            response = await tmdbApi.getMoviesList(movieType.popular, {
+              params,
+            });
+        }
+      } else {
+        const params = {
+          query: keyword,
+        };
+        response = await tmdbApi.search(props.category, { params });
+      }
+      setItems(response.results);
+      setTotalPage(response.total_pages);
+    };
+    getList();
+  }, [props.category, keyword]);
+
+  return (
+    <div >
+      {items.map((item, index) => {
+        return <MovieCard key={index} category={props.category} item={item} />;
+      })}
+    </div>
+  );
 };
 
 export default MovieLayout;
